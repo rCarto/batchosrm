257dd2700d392276ef4fec46ee5c9232ca157f76
rCarto <timothee.giraud@cnrs.fr>
2025-09-09 14:16:09 +0200

    refactor: refactor the whole package to use asynchronous requests and parallelism

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+# batchosrm 0.2.0
+
+The whole package has been refactored to use asynchronous queries and parallelism. 
+New function get_routes() has been added. 
--- R/main.R
+++ R/main.R
@@ -1,38 +1,49 @@
-#' @title Get Routes
+#' @title Get Routes with Asynchronous Requests and Parallelism
 #' @description
-#' This function enables the fast computation of routes.
+#' This function enables the fast computation of routes with async requests and
+#' parallelism.
 #'
-#' @param x dataframe with x_src, y_src, x_dst, y_dst in WGS84 coordinates
+#' @param x data.frame with x_src, y_src, x_dst, y_dst in WGS84 coordinates
 #' (EPSG:4326)
-#' @param overview overview (FALSE, "simplified" or "full")
+#' @param overview if FALSE only distance and duration are computed and the
+#' output is a matrix; if TRUE the "full" geometry is also computed and the
+#' result is an sf object.
+#' @param max_radius maximum distance between input coordinates and the road
+#' network. If this distance is exceeded, then the road is not computed.
 #' @param nc number of CPU cores
 #' @param nq number of queries per chunk
 #' @param server OSRM server address
 #' @param profile OSRM profile
 #' @importFrom foreach foreach %dopar%
 #' @export
-#' @return An sf object or a data.frame is returned.
+#' @return An sf object or a matrix is returned.
 #' @examples
 #' \dontrun{
-#' library(osrm)
-#' apt <- read.csv(system.file("csv/apotheke.csv", package = "osrm"))
-#' x <- cbind(apt[c(1:100,1:100,1:100, 1:100), c(2:3)],
-#'            apt[c(rep(1,100), rep(10,100), rep(20,100), rep(30, 100)), c(2:3)])
-#' server = "http://xxxxxxx:5000/"
-#' r <- routes(x, nc = 8, nq = 50, server = server, overview = "full")
+#' x <- read.csv(system.file("csv/berlin.csv", package = "batchosrm"))
+#' res <- routes(
+#'   x = x[1:2000,],
+#'   overview = TRUE,
+#'   max_radius = 1000,
+#'   nc = 8,
+#'   nq = 150,
+#'   server = "http://0.0.0.0:5000/",
+#'   profile = "car"
+#' )
+#'
+#' library(sf)
 #' library(mapsf)
-#' op <- par(mfrow = c(2,2))
-#' mf_map(r[1:100,], col = "red")
-#' mf_map(r[101:200,], col = "blue")
-#' mf_map(r[201:300,], col = "darkgreen")
-#' mf_map(r[301:400,], col = "purple")
-#' par(op)
+#' mf_theme("dracula")
+#' st_transform(res, "EPSG:3035") |>
+#'   mf_map(col = "#ffffff05", lwd = 1.2)
+#' mf_title("Berlin")
 #' }
 routes <- function(x,
-                   overview = "simplified",
-                   nc = 1, nq = 100,
-                   server,
-                   profile = "car"){
+                   overview = FALSE,
+                   max_radius = 1000,
+                   nc = 1,
+                   nq = 150,
+                   server = "http://0.0.0.0:5000/",
+                   profile = "car") {
   ny <- nrow(x)
   sequence <- unique(c(seq(1, ny, nq), ny + 1))
   lseq <- length(sequence) - 1
@@ -41,92 +52,112 @@ routes <- function(x,
     ml[[i]] <- as.matrix.data.frame(x[(sequence[i]):(sequence[i + 1] - 1),])
   }
 
-  cl <- parallel::makeCluster(nc, setup_strategy = "sequential")
+  cl <- parallel::makeCluster(nc)
   doParallel::registerDoParallel(cl)
   on.exit(parallel::stopCluster(cl))
 
+  res <- foreach(x = ml, .combine = rbind, .inorder = FALSE,
+                 .export = c("get_routes")) %dopar%
+    {
+      get_routes(x, overview, max_radius, server, profile)
+    }
 
-  if(overview == FALSE){
-    res <- foreach(x = ml, .combine = rbind, .inorder = FALSE,
-                   .export = c("bq", "cp")) %dopar%
-      {
-        t(sapply(apply(x, 1, bq, server = server, profile = profile, overview = "full"), cp))
-      }
-
-
-  } else {
-    res <- foreach(x = ml, .combine = rbind, .inorder = FALSE,
-                   .export = c("bq", "cpgeom")) %dopar%
-      {
-        do.call(rbind, lapply(apply(x, 1, bq, server = server, profile = profile, overview = "full"), cpgeom))
-      }
-  }
   return(res)
 
 }
 
 
 
-
-
-
-
-
-bq <- function(x, server, profile, overview){
+#' @title Get Routes with Asynchronous Requests and Parallelism
+#' @description
+#' This function enables the fast computation of routes with async requests.
+#'
+#' @param x data.frame with x_src, y_src, x_dst, y_dst in WGS84 coordinates
+#' (EPSG:4326)
+#' @param overview if FALSE only distance and duration are computed and the
+#' output is a matrix; if TRUE the "full" geometry is also computed and the
+#' result is an sf object.
+#' @param max_radius maximum distance between input coordinates and the road
+#' network. If this distance is exceeded, then the road is not computed.
+#' @param server OSRM server address
+#' @param profile OSRM profile
+#' @export
+#' @return An sf object or a matrix is returned.
+#' @examples
+#' \dontrun{
+#' x <- read.csv(system.file("csv/berlin.csv", package = "batchosrm"))
+#' res <- get_routes(
+#'   x = x[1:2000,],
+#'   overview = TRUE,
+#'   max_radius = 1000,
+#'   server = "http://0.0.0.0:5000/",
+#'   profile = "car"
+#' )
+#'
+#' library(sf)
+#' library(mapsf)
+#' mf_theme("dracula")
+#' st_transform(res, "EPSG:3035") |>
+#'   mf_map(col = "#ffffff05", lwd = 1.2)
+#' mf_title("Berlin")
+#' }
+get_routes <- function(x, overview = FALSE, max_radius = 1000, server = "http://0.0.0.0:5000/", profile = "car"){
   x <- round(x, 5)
-  utils::URLencode(paste0(server, "route/v1/", profile, "/",
-                          x[1],",",x[2], ";",x[3], ",", x[4],
-                          "?alternatives=false&geometries=polyline&steps=false&overview=",
-                          tolower(overview),
-                          "&generate_hints=false"))
+  urls <- paste0(server, "route/v1/", profile, "/",
+                 x[,1],",",x[,2], ";",x[,3], ",", x[,4], "?overview=full")
+  cc <- crul::Async$new(urls = urls,
+                        opts = list(verbose = FALSE, useragent = "batchosrm",
+                                    connecttimeout = 1000000),
+                        headers = list("Content-Type" = "application/json"))
+  res <- cc$get()
 
-
+  f <- extract_route(overview)
+  return(do.call(rbind, lapply(res, f, max_radius)))
 }
 
-cp <- function(q, req_handle){
-  tryCatch(
-    expr = {
-      req_handle <- curl::new_handle(verbose = FALSE)
-      curl::handle_setopt(req_handle, useragent = "osrm_R_package")
-      r <- curl::curl_fetch_memory(q, handle = req_handle)
-      res <- RcppSimdJson::fparse(rawToChar(r$content))
-      if (res$code != "Ok") {
-        return(c(duration = NA, distance = NA))
-      }
-      round(c(duration = res$routes$duration / 60, distance = res$routes$distance / 1000),2)
-    },
-    error = function(cond){
-      return(c(duration = NA, distance = NA))
-    }
-  )
-}
 
-cpgeom <- function(q, req_handle){
-  tryCatch(
-    expr = {
-      req_handle <- curl::new_handle(verbose = FALSE)
-      curl::handle_setopt(req_handle, useragent = "osrm_R_package")
-      r <- curl::curl_fetch_memory(q, handle = req_handle)
-      res <- RcppSimdJson::fparse(rawToChar(r$content))
-      if (res$code != "Ok") {
-        return(sf::st_sf(duration = NA, distance = NA,
-                         geometry = sf::st_sfc(sf::st_linestring()),
-                         crs = "EPSG:4326"))
-      }
-      geodf <- googlePolylines::decode(res$routes$geometry)[[1]][, c(2, 1)]
-      # Convert to LINESTRING
-      rcoords <- paste0(geodf$lon, " ", geodf$lat, collapse = ", ")
-      sf::st_sf(
-        duration = round(res$routes$duration / 60, 2),
-        distance = round(res$routes$distance / 1000, 2),
-        geometry = sf::st_as_sfc(paste0("LINESTRING(", rcoords, ")")),
-        crs = "EPSG:4326")
-
-    },
-    error = function(cond){
-      sf::st_sf(duration = NA, distance = NA,
-                geometry = sf::st_sfc(sf::st_linestring()),
-                crs = "EPSG:4326")
+extract_route <- function(overview){
+  if (isFALSE(overview)){
+    function(r, max_radius){
+      tryCatch(
+        expr = {
+          res <- RcppSimdJson::fparse(rawToChar(r$content))
+          if (any(res$code != "Ok", res$waypoints$distance >=  max_radius )) {
+            return(c(duration = NA, distance = NA))
+          }
+          return(round(c(duration = res$routes$duration / 60,
+                  distance = res$routes$distance / 1000),2))
+        },
+        error = function(cond){
+          return(c(duration = NA, distance = NA))
+        }
+      )
     }
-  )
+  } else {
+    function(r, max_radius){
+      tryCatch(
+        expr = {
+          res <- RcppSimdJson::fparse(rawToChar(r$content))
+          if (any(res$code != "Ok", res$waypoints$distance >=  max_radius )) {
+            return(sf::st_sf(duration = NA,
+                             distance = NA,
+                             geometry = sf::st_sfc(sf::st_linestring()),
+                             crs = "EPSG:4326"))
+          }
+          geodf <- googlePolylines::decode(res$routes$geometry)[[1]][, c(2, 1)]
+          sf::st_sf(
+            duration = round(res$routes$duration / 60, 2),
+            distance = round(res$routes$distance / 1000, 2),
+            geometry = sf::st_as_sfc(paste0("LINESTRING(", paste0(geodf$lon, " ", geodf$lat, collapse = ", "), ")")),
+            crs = "EPSG:4326")
+        },
+        error = function(cond){
+          sf::st_sf(duration = NA,
+                    distance = NA,
+                    geometry = sf::st_sfc(sf::st_linestring()),
+                    crs = "EPSG:4326")
+        }
+      )
+    }
+  }
 }
